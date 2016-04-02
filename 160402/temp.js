0fce8465fb461a25297ee8f2148dac34f6332ac5
Suho Kim <suho.kim2@gmail.com>
2016-04-02 16:07:21 +0900

    Update temp.js

--- 160402/temp.js
+++ 160402/temp.js
@@ -1,5 +1,12 @@
-angular.module('myApp', [])
-    .controller('MainCtrl', function($scope) {
+angular.module('myApp', ['csCenter','chart.js'])
+    .value('SERVER_URL', 'http://www.daum.net')
+    .controller('RootCtrl', function($scope){
+        //$scope.name = '루트컨트롤러!! 바디다!!!';
+
+    })
+    .controller('MainCtrl', function($scope, CallService) {
+        $scope.name = '메인컨트롤러';
+
         var bookmarks = localStorage.getItem('bookmarks');
         var parsedBookmarks = JSON.parse(bookmarks);
         $scope.bookmarks = parsedBookmarks || [];
@@ -20,4 +27,28 @@ angular.module('myApp', [])
                 JSON.stringify($scope.bookmarks)
             );
         };
-    });
+    })
+    .controller('SecondCtrl', function($scope, $log, SERVER_URL, CallService) {
+        $scope.server = SERVER_URL;
+    })
+    .controller('ChartCtrl', function($scope){
+        $scope.data = [
+            [123,234,345,111,11,97,43],
+            [112,444,123,166,421,522,11],
+            [44,11,345,22,11,111,43]
+        ];
+        $scope.dd = [44,11,345,22,11,111,43];
+        $scope.legend = true;
+        $scope.series = ['A', 'B','C'];
+        $scope.labels = ['월','화','수','목','금','토','일'];
+    })
+
+
+
+
+
+
+
+
+
+
