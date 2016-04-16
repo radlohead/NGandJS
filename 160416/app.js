6a47d809bf210e5c65c4097094323aa94b135367
Suho Kim <suho.kim2@gmail.com>
2016-04-16 15:48:30 +0900

    Update app.js

--- 160416/app.js
+++ 160416/app.js
@@ -11,9 +11,34 @@ angular.module('weatherApp', ['ngRoute', 'ngResource'])
                 controller : 'ForecastCtrl'
             })
     })
-    .controller('HomeCtrl', function($scope) {
+    .service('CityService', function(){
+        this.city = 'Seoul';
+    })
+    .controller('HomeCtrl', function($scope, CityService) {
+        $scope.city = CityService.city;
+
+        $scope.$watch('city', function(newCity){
+            CityService.city = newCity;
+        })
 
     })
-    .controller('ForecastCtrl', function($scope) {
+    .controller('ForecastCtrl', function($scope, CityService, $resource) {
+        $scope.city = CityService.city;
+        $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily',{
+            callback : 'JSON_CALLBACK'
+        },{
+            get : {
+                method : 'JSONP'
+            }
+        });
+        $scope.weatherResult = $scope.weatherAPI.get({q:$scope.city, cnt: 2});
+        console.log($scope.weatherResult)
+        $scope.convertToDate = function(dt) {
+            return new Date(dt * 1000);
+        };
+
+        $scope.convertToCelsius = function(degK){
+            return Math.round(degK - 273.15);
+        };
 
     })
